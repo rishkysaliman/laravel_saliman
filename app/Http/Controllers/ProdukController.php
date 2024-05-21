8ce2b63f74663e5d7eeabe9ed20c3fc6a4a0d197
rishkysaliman <rishkysaliman24@gmail.com>
2024-05-21 14:46:54 +0700

    push yang pertama

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produk;
+use Illuminate\Http\Request;
+use PDF;
+use Storage;
+
+class ProdukController extends Controller
+{
+
+    public function viewPDF()
+    {
+        $produk = Produk::latest()->get();
+
+        $data = [
+            'title' => 'Data Produk',
+            'date' => date('m/d/Y'),
+            'produk' => $produk,
+        ];
+
+        $pdf = PDF::loadView('produk.export-pdf', $data)
+            ->setPaper('a4', 'portrait');
+        return $pdf->stream();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produk = Produk::latest()->paginate(5);
+        return view('produk.index', compact('produk'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('produk.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nama' => 'required|min:5',
+            'harga' => 'required',
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'deskripsi' => 'required|min:10',
+        ]);
+
+        $produk = new Produk();
+        $produk->nama = $request->nama;
+        $produk->harga = $request->harga;
+        $produk->deskripsi = $request->deskripsi;
+        // upload gambar
+        $image = $request->file('image');
+        $image->storeAs('public/produks', $image->hashName());
+        $produk->image = $image->hashName();
+        $produk->save();
+        return redirect()->route('produk.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $produk = Produk::findOrFail($id);
+      return view('produk.show', compact('produk'));
+    }   
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $produk = Produk::findOrFail($id);
+      return view('produk.edit', compact('produk'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'nama' => 'required|min:5',
+            'harga' => 'required',
+            'deskripsi' => 'required|min:10',
+        ]);
+
+        $produk = Produk::findOrFail($id);
+        $produk->nama = $request->nama;
+        $produk->harga = $request->harga;
+        $produk->deskripsi = $request->deskripsi;
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/produks', $image->hashName());
+        //delete old image
+        Storage::delete('public/produks/' . $produk->image);
+
+        $produk->image = $image->hashName();
+        $produk->save();
+        return redirect()->route('produk.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $produk = Produk::findOrFail($id);
+      Storage::delete('public/produks/' . $produk->image);
+      $produk->delete();
+      return redirect()->route('produk.index');
+    }
+}
